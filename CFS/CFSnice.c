9ddca35ecd06dc68cd0e2a025345e0d4a318eb99
durumi99 <43162188+durumi99@users.noreply.github.com>
2021-12-23 21:16:47 +0900

    Add files via upload

---
+++ CFS/CFSnice.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+
+int main(){
+	int status;
+	int pid[21];
+	for(int i = 0 ; i < 21 ; i++){
+		if( ( pid[i] = fork() ) == 0){
+			printf("%d process begins\n",getpid());
+			if( i < 7)
+				nice(19);
+			else if( i < 14)
+				nice(0);
+			else
+				nice(-19);
+			int tmp = 1;
+			for(int i = 0 ; i < 99999999; i++)
+				tmp *= tmp;
+			printf("%d process ends\n",getpid());
+			exit(100+i);
+		}
+	}
+	for(int i = 0 ; i < 21 ; i++) {
+		pid_t wpid = waitpid(pid[i],&status,0);
+		}
+	printf("---------- All processes end ----------\n");
+	return 0;
+}
